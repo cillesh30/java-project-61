04245c8c57ead21f57ae3ab634a0e91de0fc1bc0
cillesh30 <cillesh30@gmail.com>
2024-08-01 15:44:01 +0200

    Upgade file Calc for Sergey recomendation

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -20,32 +20,59 @@ public class Calc {
         return operate[index];
     }
 
-    public static void calcGame() {
-        final var description = "What is the result of the expression?";
-
+    public static int someNumber1() {
         Random random = new Random();
-        int i = 0;
+        int randomNumber1 = random.nextInt(MAX_RANDOM_NUMBER_1);
+//
+        return randomNumber1;
+    }
 
+    public static int someNumber2() {
+        Random random = new Random();
+        int randomNumber2 = random.nextInt(MAX_RANDOM_NUMBER_2);
+//        String StringRandomNumber2 = Integer.toString(randomNumber2);
+        return randomNumber2;
+    }
+
+    private static String calculate(int randomNumber1, int randomNumber2, char operate) {
+        int result = 0;
+
+        if (operate == '+') {
+            result = randomNumber1 + randomNumber2;
+        } else if (operate == '-') {
+            result = randomNumber1 - randomNumber2;
+        } else if (operate == '*') {
+            result = randomNumber1 * randomNumber2;
+        }
+        String stringResult = Integer.toString(result);
+        return stringResult;
+    }
+
+    public static void createArray() {
+        final var description = "What is the result of the expression?";
+        int i = 0;
         while (i < GAME_COUNT) {
-            int randomNumber1 = random.nextInt(MAX_RANDOM_NUMBER_1);
-            int randomNumber2 = random.nextInt(MAX_RANDOM_NUMBER_2);
-            int result = 0;
+            int randomNumber1 = someNumber1();
+            String stringRandomNumber1 = Integer.toString(randomNumber1);
+            int randomNumber2 = someNumber2();
+            String stringRandomNumber2 = Integer.toString(randomNumber2);
             char operate = someOperate();
 
-            if (operate == '+') {
-                result = randomNumber1 + randomNumber2;
-            } else if (operate == '-') {
-                result = randomNumber1 - randomNumber2;
-            } else if (operate == '*') {
-                result = randomNumber1 * randomNumber2;
-            }
-
-            String stringResult = Integer.toString(result);
-            questionAndAnswerArray[i][0] = "Question: " + randomNumber1 + " " + operate + " " + randomNumber2;
+            String stringResult = calculate(randomNumber1, randomNumber2, operate);
+            questionAndAnswerArray[i][0] = "Question: "
+                    + stringRandomNumber1
+                    + " "
+                    + operate
+                    + " "
+                    + stringRandomNumber2;
             questionAndAnswerArray[i][1] = stringResult;
             i++;
 
         }
         Engine.makeAnswers(description, getQuestionAndAnswerArray());
+    }
+
+    public static void calcGame() {
+        Calc.createArray();
     }
 }
