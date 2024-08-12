691d767c4a800f557982bf728aab6c7031b06e2a
cillesh30 <cillesh30@gmail.com>
2024-08-13 00:37:34 +0200

    Fixed mistake in file Engine

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -34,7 +34,7 @@ public class Engine {
                 System.out.println("Let's try again, " + userName + "!");
                 return;
             }
-            System.out.println("Congratulations, " + userName + "!");
         }
+        System.out.println("Congratulations, " + userName + "!");
     }
 }
